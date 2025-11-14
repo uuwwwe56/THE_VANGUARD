75e89b654c724359bc36577239ed1e06e31ce9d6
uuwwwe56 <alifsaptam@gmail.com>
2025-11-14 08:47:32 +0700

    produk 30%

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".card-slider").forEach((slider) => {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    let isDragging = false;
+
+    slider.addEventListener("mousedown", (e) => {
+      isDown = true;
+      isDragging = false;
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener("mouseleave", () => {
+      isDown = false;
+    });
+
+    slider.addEventListener("mouseup", () => {
+      isDown = false;
+      setTimeout(() => (isDragging = false), 50);
+    });
+
+    slider.addEventListener("mousemove", (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      isDragging = true;
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 1.5;
+      slider.scrollLeft = scrollLeft - walk;
+    });
+
+    slider.querySelectorAll("a, button").forEach((el) => {
+      el.addEventListener("click", function (e) {
+        if (isDragging) {
+          e.preventDefault();
+          e.stopImmediatePropagation();
+        }
+      });
+    });
+  });
+});
